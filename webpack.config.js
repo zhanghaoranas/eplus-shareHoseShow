360e73e94abc61a7a9cbd8eca91664b36e0c6bbf
zhr <zhr@qq.com>
2020-05-07 11:52:19 +0800

    修改配置文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,92 +1,103 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {
-	CleanWebpackPlugin
-} = require('clean-webpack-plugin');
-const webpack = require('webpack');
-const devMode = process.env.NODE_ENV === 'development';
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
+const webpack = require("webpack");
+const devMode = process.env.NODE_ENV === "development";
 
 module.exports = {
-	entry: './src/index.jsx',
-	devtool: devMode ? 'inline-source-map' : '',
+	entry: "./src/index.jsx",
+	devtool: devMode ? "inline-source-map" : "",
 	devServer: {
 		historyApiFallback: true,
-		contentBase: path.join(__dirname, 'dist'),
+		contentBase: path.join(__dirname, "dist"),
 		port: 8080,
-		hot: true
+		hot: true,
+		quiet: true,
 	},
-	mode: devMode ? 'development' : 'production',
+	mode: devMode ? "development" : "production",
 	module: {
-		rules: [{
-			test: /\.css$/,
-			exclude: /node_modules/,
-			use: [{
-					loader: MiniCssExtractPlugin.loader,
-					options: {
-						esModule: true,
-						hmr: devMode,
-					}
-				},
-				{
-					loader: 'css-loader',
-					options: {
-						modules: {
-							localIdentName: '[name]__[local]--[hash:base64:5]'
+		rules: [
+			{
+				test: /\.css$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							esModule: true,
+							hmr: devMode,
 						},
-					}
-				},
-				'postcss-loader',
-			]
-		}, {
-			test: /\.css$/,
-			include: /node_modules/,
-			use: [{
-					loader: MiniCssExtractPlugin.loader,
-					options: {
-						esModule: true,
-						hmr: devMode,
-					}
-				},
-				'css-loader',
-				'postcss-loader',
-			]
-		}, {
-			test: /\.(jsx|js)$/,
-			exclude: /node_modules/,
-			loader: "babel-loader"
-		}, {
-			test: /\.(png|svg|jpg|gif)$/,
-			use: [{
-				loader: 'file-loader',
-				options: {
-					outputPath: 'images'
-				}
-			}]
-		}],
-
+					},
+					{
+						loader: "css-loader",
+						options: {
+							modules: {
+								localIdentName:
+									"[name]__[local]--[hash:base64:5]",
+							},
+						},
+					},
+					"postcss-loader",
+				],
+			},
+			{
+				test: /\.css$/,
+				include: /node_modules/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							esModule: true,
+							hmr: devMode,
+						},
+					},
+					"css-loader",
+					"postcss-loader",
+				],
+			},
+			{
+				test: /\.(jsx|js)$/,
+				exclude: /node_modules/,
+				loader: "babel-loader",
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							outputPath: "images",
+						},
+					},
+				],
+			},
+		],
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			title: '移动端 webpack搭建',
-			template: 'template/index.html'
+			title: "移动端 webpack搭建",
+			template: "template/index.html",
 		}),
 		new webpack.ProgressPlugin(),
 		new CleanWebpackPlugin(),
 		new MiniCssExtractPlugin({
-			filename: devMode ? 'css/[name].css' : 'css/[name].[hash:6].css',
-			chunkFilename: devMode ? 'css/[id].css' : 'css/[id].[hash:6].css',
+			filename: devMode ? "css/[name].css" : "css/[name].[hash:6].css",
+			chunkFilename: devMode ? "css/[id].css" : "css/[id].[hash:6].css",
 		}),
+		new FriendlyErrorsWebpackPlugin(),
 		new webpack.HotModuleReplacementPlugin(),
 	],
 	optimization: {
 		splitChunks: {
-			chunks: 'all'
-		}
+			chunks: "all",
+		},
 	},
+	stats: "errors-only",
 	output: {
-		filename: 'js/bundle.[hash].js',
-		path: path.resolve(__dirname, 'dist'),
-		publicPath: '/'
+		filename: "js/bundle.[hash].js",
+		path: path.resolve(__dirname, "dist"),
+		publicPath: "/",
 	},
 };
